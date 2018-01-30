847892405eaa4373b7d9acfd3f117e3ef58bea71
Fenn.Cheng <dj99fei@gmail.com>
2018-01-30 23:11:16 +0800

    first commit

---
+++ app/src/main/java/com/dj99fei/MainActivity.java
@@ -0,0 +1,13 @@
+package com.dj99fei;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
